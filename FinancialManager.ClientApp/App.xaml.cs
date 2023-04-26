795fd484ed256b9d04702bd979774ff14fb26ac2
aeriel999 <ok4743@gmail.com>
2023-04-26 09:44:34 +0300

    Update project

--- FinancialManager.ClientApp/App.xaml.cs
+++ FinancialManager.ClientApp/App.xaml.cs
@@ -6,12 +6,10 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
 
-namespace FinancialManager.ClientApp
+namespace FinancialManager.ClientApp;
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
 {
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
-    public partial class App : Application
-    {
-    }
 }
