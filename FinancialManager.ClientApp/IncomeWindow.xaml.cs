794451674029b85752440cfcd40ee2ff6a81f66c
aeriel999 <ok4743@gmail.com>
2023-05-01 10:46:00 +0300

    Add IncomeWindows

--- FinancialManager.ClientApp/IncomeWindow.xaml.cs
+++ FinancialManager.ClientApp/IncomeWindow.xaml.cs
@@ -10,25 +10,16 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace FinancialManager.ClientApp;
 /// <summary>
-/// Interaction logic for MainWindow.xaml
+/// Interaction logic for IncomeWindow.xaml
 /// </summary>
-public partial class MainWindow : Window
+public partial class IncomeWindow : Window
 {
-    public MainWindow()
+    public IncomeWindow()
     {
         InitializeComponent();
-
-        //var win = new ResponseWindow();
-
-        //win.Show();
-
-        //this.Close();
     }
-
 }
-
